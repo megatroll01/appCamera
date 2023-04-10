f19989f4c19a6c319db98a41a04bef51a1881517
megatroll01 <54943101+megatroll01@users.noreply.github.com>
2023-04-10 10:01:28 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appCamera',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
